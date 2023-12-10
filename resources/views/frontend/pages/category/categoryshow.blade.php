f1298706831829b22b1be06fb9af47c4a661b04f
statementloop <rimizoo468@gmail.com>
2023-12-11 00:38:03 +0600

    show category

--- resources/views/frontend/pages/category/categoryshow.blade.php
+++ resources/views/frontend/pages/category/categoryshow.blade.php
@@ -1,32 +1,86 @@
 @extends('frontend.partials.webmaster')
 @section('content')
 
+
+
 <style>
-
-    .flex-box{
-
+    .container{
         display: flex;
+        justify-content: center;
+        
+    }
+
+    .cards{
+        width: 300px;
+        margin: 10px;
+        border-radius: 15px;
+        background-color: wheat;
+    }
+
+    .card-img img{
+        width: 100%;
+        border-radius: 10px 10px 0px 0px;
+    }
+
+    .card-body h2{
+        text-align: center;
+    }
+
+    .card-body p{
+        text-align: justify;
+        margin-bottom: 10px;
+        padding: 5px;
+    }
+
+    .card-footer{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .card-footer button{
+        width: 150px;
+        background: black;
+        color: white;
+        border-radius: 15px;
+        border: 2px solid black ;
+        padding: 6px;
+        font-size: 17px;
+        cursor: pointer;
+        margin-bottom: 10px;
+    }
+
+    .card-footer button:hover{
+        background-color: white;
+        color: black;
+        border: 2px solid black;
     }
 </style>
-<div class="border rounded-5">
-<section class="p-4 text-center mb-4 w-100 ">
-<div class="row row-cols-3 g-3" >
-<div class="col">
+
+
+<div class="container">
+
     @foreach($categories as $data)
+<div class="cards">
+
+    <div class="card-img">
+        <img src="{{url('/uploads/'.$data->image)}}" alt="">
+    </div>
+    <div class="card-body">
+        <h2>{{$data->name}}</h2>
+        <p>{{$data->title}}</p>
+    </div>
+    <div class="card-footer">
+        <button>posts</button>
+    </div>
     
-<a href="{{route('post.category',$data->id)}}">
-<div class="card" style="width: 8rem;">
-  <img class="card-img-top" src="{{url('/uploads/'.$data->image)}}" width=40 alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">{{$data->name}}</h5>
-    <p class="card-text">{{$data->title}}</p>
-    <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
-  </div>
 </div>
-</a>
 @endforeach
+
 </div>
-</div>
-</section>
-</div>
+
+<br>
+<br>
+<br>
+<br>
 @endsection
